ba2f03138e9b32cd4aa59572df57a17b9ed4afd1
khojiakbarkhayotov <khayotovkhojiakbar@gmail.com>
2022-10-09 21:43:36 +0500

    make it better

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ const server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+server.listen(process.env.PORT || 8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
